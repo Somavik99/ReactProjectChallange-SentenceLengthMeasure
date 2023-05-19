289b616d9642743c4864736314f7899b672f54b9
Avik Roy <somavik99@gmail.com>
2023-05-20 01:06:17 +0530

    main

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,27 @@
-
+import { AiOutlineTwitter } from "react-icons/ai";
+import { BsNewspaper } from "react-icons/bs";
+import { RiLinkedinFill } from "react-icons/ri";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <h2>Text Analyzer</h2>
-        <div>
-                <span></span>
-                <span></span>
-                <span></span>
+    <div className="Main-Header">
+      <div className="Header-container">
+        <h3 className="Heading">Text Analyzer</h3>
+        <div className="Icon-container">
+          <span style={{margin:"6px",padding:"4px"}}>
+            <BsNewspaper />
+          </span>
+          <span style={{margin:"6px",padding:"4px"}}>
+            <AiOutlineTwitter />
+          </span>
+          <span style={{margin:"6px",padding:"4px"}}>
+            <RiLinkedinFill />
+          </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
